e2cc632ccdc32de6c80cc9dae0de01dbddba50ed
ChanJoo <sksckswn@naver.com>
2017-11-09 14:46:49 -0500

    edited 3 expandable layout(doorbell, temperature, trash can) / onClick method / round corner

--- app/src/main/java/com/tistory/dayglo/smarting_android/MainActivity.java
+++ app/src/main/java/com/tistory/dayglo/smarting_android/MainActivity.java
@@ -2,13 +2,36 @@ package com.tistory.dayglo.smarting_android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.github.aakira.expandablelayout.ExpandableRelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+    ExpandableRelativeLayout doorbellLayout, temperatureLayout, trashLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
         setContentView(R.layout.activity_main);
+
+        // TODO 2017-11-09 ButterKnife 적용
+        doorbellLayout = (ExpandableRelativeLayout) findViewById(R.id.doorbell_layout);
+        temperatureLayout = (ExpandableRelativeLayout) findViewById(R.id.temperature_layout);
+        trashLayout = (ExpandableRelativeLayout) findViewById(R.id.trash_layout);
+
+
+    }
+
+    public void onClickDoorbell(View view) {
+        doorbellLayout.toggle();
+    }
+
+    public void onClickTemperature(View view) {
+        temperatureLayout.toggle();
+    }
+
+    public void onClickTrashCan(View view) {
+        trashLayout.toggle();
     }
 }
